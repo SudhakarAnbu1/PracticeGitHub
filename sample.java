81c2074f013a322333ffed3d7295b400b633b4ed
SudhakarAnbu1 <sudhakar2001dpi@gmail.com>
2024-10-15 17:48:49 +0530

    fix:additional information is added to backend part

--- sample.java
+++ sample.java
@@ -2,6 +2,7 @@ class Sample
 {
     public static void main(String[]args)
     {
-        System.out.println("This is a java folder")
+        System.out.println("This is a java folder");
+        System.out.println("This the additional statement");
     }
 }
